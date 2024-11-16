74fb842a61e5b0b8d8a61fd4d701a43008084c41
Thomas <thomasribalta@gmail.com>
2024-11-16 19:49:13 +0100

    Start with auth systeme (jwt)

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Attente de MongoDB..."
+sleep 10
+
+echo "Création de la base de données Cohelio..."
+mongo --host localhost -u root -p rootpassword --authenticationDatabase admin <<EOF
+use Cohelio
+EOF
+
+echo "Base de données 'Cohelio' créée."
